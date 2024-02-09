6b07af2f70bc9b9d9f813d91f72195d794ea952d
Malmqvist Studios <127353105+MalmqvistStudios@users.noreply.github.com>
2024-02-09 12:21:34 +0100

    Add files via upload

---
+++ malmqvist-studio-grovestreet-hood/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 "yes"
+
+author 'MotorHorizon/Malmqviststudio'
+description 'Project Grove street'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+
+
+file 'davis.ymt'
+data_file 'SCENARIO_POINTS_OVERRIDE_FILE' 'davis.ymt'
+
+file 'davis_megamall.ymt'
+data_file 'SCENARIO_POINTS_OVERRIDE_FILE' 'davis_megamall.ymt'
+
+
+
+
+
+
+
+
+
+
